0b26e646d9e989076f02a406400898dd851528ee
sara <s.kardanan@gmail.com>
2020-08-01 13:11:49 +0430

    first commit

---
+++ Session04.MassTest.ComponentConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using MassTransit;
+using Session04.MassTest.Contracts;
+using Session04.MassTest.Components.Consumers;
+namespace Session04.MassTest.ComponentConsumer
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                sbc.Host("rabbitmq://localhost");
+
+                sbc.ReceiveEndpoint("test_queue", ep =>
+                {
+                    ep.Handler<OrderRegistered>(context =>
+                    {
+                        var res = new OrderRegisteredHandler().Consume(context);
+                        return res;
+                        //return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            await bus.StartAsync();
+
+            Console.WriteLine("Press any key to exit");
+            await Task.Run(() => Console.ReadKey());
+
+            await bus.StopAsync();
+        }
+    }
+}
---
+++ Session04.MassTest.Contracts/OrderRegistered.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Session04.MassTest.Contracts
+{
+    public class OrderRegistered
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerNumber { get; set; }
+    }
+
+}
